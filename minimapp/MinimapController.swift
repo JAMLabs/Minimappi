00ffe67681c89996345e7f5657f02254bbefb6b2
acerio98 <a.c.riopelle@me.com>
2015-08-01 16:51:05 -0400

    4:50pm

--- minimapp/MinimapController.swift
+++ minimapp/MinimapController.swift
@@ -10,16 +10,36 @@ import UIKit
 import MapKit
 import QuartzCore
 
-class MinimapController: UIViewController {
+class MinimapController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var searchBar: UISearchBar!
+    var searchBar = UISearchBar(frame: CGRectMake(-5.0, 0.0, 320.0, 44.0))
+    
+    var boundingRegion : MKCoordinateRegion = MKCoordinateRegion()
+    var localSearch : MKLocalSearch = MKLocalSearch()
+    var locationManager : CLLocationManager = CLLocationManager()
+    var userLocation : CLLocationCoordinate2D = CLLocationCoordinate2D()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.grayColor()
         mapView.layer.cornerRadius = 140.0
+        
+
+        searchBar.autoresizingMask = UIViewAutoresizing.FlexibleWidth
+        var searchBarView = UIView(frame: CGRectMake(0.0, 0.0, 340.0, 44.0))
+        searchBarView.autoresizingMask = UIViewAutoresizing.allZeros
+        searchBar.delegate = self
+        searchBarView.addSubview(searchBar)
+        self.navigationItem.titleView = searchBarView;
+        
+        var tapGest = UITapGestureRecognizer(target: self, action: "hideSearchBar")
+        self.view.addGestureRecognizer(tapGest)
+        
+        locationManager.delegate = self
+        locationManager.startUpdatingHeading()
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +47,9 @@ class MinimapController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func hideSearchBar(){
+        searchBar.resignFirstResponder()
+    }
+    
 }
 
